7fafe0b39ae9185f8d63b3ef7ea35cfc49678c88
dweinberger <david@weinberger.org>
2016-01-24 21:14:27 -0500

    added awards date

---
+++ oscarPage.php
@@ -0,0 +1,25 @@
+<?php
+
+$f = file_get_contents("./oscars.json.2015/oscars2015.json");
+//echo $f;
+$j = json_decode($f);
+$noms = $j->nominations;//[0][categories;
+$ct = count($noms);
+echo $ct;
+for ($i=0;$i < $ct; $i++){
+	$cat = $noms[$i]->category->category_name;
+	echo $cat;
+
+}
+
+//echo $j["nominees"][0]['category']['category_name'];
+
+// foreach ($j as $key => $value) {
+// 	echo $j['category']['category_name'];
+//           
+//         }
+
+?>
+
+
+
